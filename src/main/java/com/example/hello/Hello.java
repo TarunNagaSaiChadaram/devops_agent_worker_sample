185e3f5d4a4da270c1164dc690dadc18cc7e87fd
TarunNagaSaiChadaram <162387068+TarunNagaSaiChadaram@users.noreply.github.com>
2025-10-19 14:15:16 -0400

    Update Hello.java

--- src/main/java/com/example/hello/Hello.java
+++ src/main/java/com/example/hello/Hello.java
@@ -3,6 +3,5 @@ package com.example.hello;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello from Jenkins worker running Maven build!");
-        System.out.println("Hello This build is from Tarun");
     }
 }
